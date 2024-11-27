b5c4ac8396cd555ab3574ab7f0a1dbcdc69d857e
stevendev97 <x.limsteven@gmail.com>
2024-11-27 13:11:40 -0600

    bubble sort

--- mergeArr.js
+++ mergeArr.js
@@ -1,5 +1,27 @@
+const bubbleSort = (arr) => {
+    let swapped
+    do {
+        swapped = false
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i + 1]
+                arr[i + 1] = arr[i]
+                arr[i] = temp
+                swapped = true
+            }
+        }
+
+    } while (swapped)
+
+}
+
 const mergeArr = (arr1, arr2) => {
-    return [...arr1, ...arr2].sort((a,b) => a - b)
+
+    const result = [...arr1, ...arr2]
+    bubbleSort(result)
+
+    return result
+
 }
 
 console.log(mergeArr([1,3,5],[2,4,6]))
